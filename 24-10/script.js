f1f3a704193cb45bd82bd79053fa524727a55b82
jumabekova571 <aizada.dadaday@gmail.com>
2022-10-27 01:13:08 +0600

    26-10

---
+++ 24-10/script.js
@@ -0,0 +1,10 @@
+let name = 'Aйзада';
+let age = 17;
+let str1 = 'САЛАМ';
+let str2 = 'БИШКЕК';
+
+alert(`Привет, ${name}!`)
+alert(` Мне, ${age} лет! `)
+alert(`${str1},${str2}!`)
+
+
---
+++ 25-10/script.js
@@ -0,0 +1,25 @@
+let a = 17;
+let b = 10;
+let test = a - b;
+console.log(test)
+
+let c = 7;
+let d = 7;
+let z  = c * d;
+console.log(z);
+
+let result = 49;
+alert (result);
+
+
+let  hour = 11;
+let  min = 13;
+let sec = 25;
+alert(`${hour}:${min}:${sec}:`);
+
+var num = 10;
+num++;
+alert(num);
+
+
+
