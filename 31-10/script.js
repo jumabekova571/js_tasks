e21d4c4cf29e33d01e493218ff422c8c7881c298
jumabekova571 <aizada.dadaday@gmail.com>
2022-10-31 21:59:37 +0600

    31-10

---
+++ 31-10/script.js
@@ -0,0 +1,34 @@
+/******* Задание-1 ****
+
+const userSalaries = {
+    Ella: 10000,
+    Sophia:15000,
+    Ellie:18000,
+};
+
+alert(userSalaries.Ella);
+alert(userSalaries.Sophia);
+
+
+/!******** Задание-2  *********!/
+ let age = {
+     a:5,
+     b:6,
+     c:10
+};
+
+ let sum = 0;
+ for (let key in age) {
+     sum += age[key];
+ }
+ alert(sum);*/
+
+/******** Задание-3  *********/
+let data;
+console.log(data)
+let number = data === true ? 3 : 5;
+console.log(number)
+
+
+
+
